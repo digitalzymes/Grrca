0e40f15c0f46ff70b326e7e0523e56db383f0848
Rohan Rawat <rohan@Rohans-MacBook-Air.local>
2025-04-11 13:10:35 +0530

    testing frontend

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.set("views", path.resolve("./views"));
 // Middleware
 app.use(
   cors({
-    origin: "https://grrcaindia.com/" || "http://127.0.0.1:5500",
+    origin: "http://127.0.0.1:5500",
     credentials: true,
   })
 );
